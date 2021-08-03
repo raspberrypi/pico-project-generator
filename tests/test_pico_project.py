3ba88809e694d5e63247ee389c53a059779fe8ba
matiasilva <matias.silva@raspberrypi.com>
2021-08-03 17:23:26 +0100

    Add basic integration test

--- tests/test_pico_project.py
+++ tests/test_pico_project.py
@@ -1,2 +1,8 @@
-def test_basic_project():
-    assert 1 == 1
+import subprocess
+
+# basic integration testing
+def test_basic_project(tmp_path):
+    test_path = str(tmp_path / "test_project")
+    completed = subprocess.run(["python3", "pico_project.py", test_path])
+    assert completed.returncode == 0
+
