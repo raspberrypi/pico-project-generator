f3b5ddb74afa814f095eb7c6555dcaea1c455d23
matiasilva <matias.silva@raspberrypi.com>
2021-08-04 14:42:17 +0100

    Separate logic into files

---
+++ pico_project/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#
+# Copyright (c) 2021 Raspberry Pi (Trading) Ltd.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+from cli import get_args
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ pico_project/cli.py
@@ -0,0 +1,39 @@
+#
+# Copyright (c) 2021 Raspberry Pi (Trading) Ltd.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+"""
+cli -- configuration for the pico-project-generator command line interface
+"""
+
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Pico Project generator')
+    parser.add_argument("name", nargs="?", help="Name of the project")
+    parser.add_argument("-t", "--tsv", help="Select an alternative pico_configs.tsv file", default="pico_configs.tsv")
+    parser.add_argument("-o", "--output", help="Set an alternative CMakeList.txt filename", default="CMakeLists.txt")
+    parser.add_argument("-x", "--examples", action='store_true', help="Add example code for the Pico standard library")
+    parser.add_argument("-l", "--list", action='store_true', help="List available features")
+    parser.add_argument("-c", "--configs", action='store_true', help="List available project configuration items")
+    parser.add_argument("-f", "--feature", action='append', help="Add feature to generated project")
+    parser.add_argument("-over", "--overwrite", action='store_true', help="Overwrite any existing project AND files")
+    parser.add_argument("-b", "--build", action='store_true', help="Build after project created")
+    parser.add_argument("-g", "--gui", action='store_true', help="Run a GUI version of the project generator")
+    parser.add_argument("-p", "--project", action='append', help="Generate projects files for IDE. Options are: vscode")
+    parser.add_argument("-r", "--runFromRAM", action='store_true', help="Run the program from RAM rather than flash")
+    parser.add_argument("-uart", "--uart", action='store_true', default=1, help="Console output to UART (default)")
+    parser.add_argument("-nouart", "--nouart", action='store_true', default=0, help="Disable console output to UART")
+    parser.add_argument("-usb", "--usb", action='store_true',
+                        help="Console output to USB (disables other USB functionality")
+    parser.add_argument("-cpp", "--cpp", action='store_true', default=0, help="Generate C++ code")
+    parser.add_argument("-cpprtti", "--cpprtti", action='store_true',
+                        default=0, help="Enable C++ RTTI (Uses more memory)")
+    parser.add_argument("-cppex", "--cppexceptions", action='store_true',
+                        default=0, help="Enable C++ exceptions (Uses more memory)")
+    parser.add_argument("-d", "--debugger", type=int, help="Select debugger (0 = SWD, 1 = PicoProbe)", default=0)
+
+    return parser.parse_args()
+
