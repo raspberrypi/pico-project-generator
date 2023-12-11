837fe5d07bbf6ab3e5bfc5aa0d61fc3a2735d9f2
BitRolher <153378650+BitRolher@users.noreply.github.com>
2023-12-11 16:53:30 +0100

    Enhanced launcher script

--- launch_with_pydiscovery.cmd
+++ launch_with_pydiscovery.cmd
@@ -1,25 +1,44 @@
 @if not defined _echo echo off
+rem save path of the launcher script
 set "lscp-path=%cd%"
 
 rem https://stackoverflow.com/questions/22352793/reading-a-registry-value-to-a-batch-variable-handling-spaces-in-value
+rem Find Regkey of Raspberry Pi Pico SDK
+for /f "usebackq tokens=1,*" %%h in (
+  `"reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" /f "Raspberry Pi Pico SDK" 2>NUL | find /i "Raspberry Pi Pico SDK""`
+  ) do (
+  set "pico-sdk-regkey=%%h %%i"
+)
+
+rem Find path to Raspberry Pi Pico SDK
 for /f "usebackq tokens=2,*" %%h in (
-  `"reg query "HKLM\SOFTWARE\Kitware\CMake\Packages\pico-sdk-tools" 2>NUL | find /i "SDK""`
+  `"reg query "%pico-sdk-regkey%" /v "InstallPath" 2>NUL"`
   ) do (
   set "pico-env-for-projGen-path=%%i"
 )
 
+rem Delete no longer required variable
+set pico-sdk-regkey=
+
+rem change directory to prepare the environment by launching the existing script pico-env
 cd /D "%pico-env-for-projGen-path%"
-cd ..
+rem Delete no longer required variable
 set "pico-env-for-projGen-path="
 call pico-env.cmd
 
+rem change directory back to launcher script
 cd /D %lscp-path%
+
+rem Delete no longer required variable
 set "lscp-path="
 
 REM https://blog.finxter.com/how-to-find-path-where-python-is-installed-on-windows/
-py.exe -c "import os, sys; print(os.path.dirname(sys.executable))" > lspver.tmp
-set /p ls-pypath= < lspver.tmp
-del lspver.tmp
-call "%ls-pypath%\python" pico_project.py --gui
-set ls-pypath=
+for /f "usebackq tokens=2,*" %%h in (
+  `py.exe -c "import os, sys; print('Python Path ' + os.path.dirname(sys.executable))"`
+  ) do (
+  set "ls-pypath=%%i"
+)
 
+call "%ls-pypath%\python" pico_project.py --gui
+rem Delete no longer required variable
+set ls-pypath=
